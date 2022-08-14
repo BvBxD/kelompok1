69e65756ec8213b00317a176ac446b09e9311ceb
bvbxd <maulanahusenn@gmail.com>
2022-08-14 18:40:45 +0700

    update js, navbar

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 function addContent(params) {
     let container = document.getElementById("container");
+<<<<<<< HEAD
     if (params == "contact") {
         container.innerHTML = `        
         <form action="">
@@ -8,12 +9,36 @@ function addContent(params) {
             PESAN : <textarea name="" id="" cols="30" rows="10"></textarea>
         </form>`;
     } else if (params == "profile") {
+=======
+    if (params == "History") {
+        container.innerHTML = `        
+        <div class="right">
+            <h1>SEJARAH</h1>
+            <br />
+            <h2 style="color: #4ec5c1">
+                Kenapa Isi Navbar Menjadi History <br />
+                Bukan Profil/Contact?
+            </h2>
+            <br />
+            <p>
+                Awalnya bagian navbar ini buat contact & profile tapi ngga jadi karena
+                saya nggak bisa makai iframe jadilah pakai innerHTML. pas sudah saya
+                bingung desainnya kaya gimana, jadilah saya di kebawahin sedangkan
+                navbar jadi bagian history & about yang isinya cuma tulisan aja
+            </p>
+        </div>
+        <div class="left">
+            <img src="assets/arkeolog.webp" alt="" width="500px" height="375px" />
+        </div>`;
+    } else if (params == "about") {
+>>>>>>> 6e8dd41 (update js, navbar)
         container.innerHTML = `
             <h1>INI PROFILE</h1>
         `;
     } else if (params == "home") {
         container.innerHTML = `
         <div class="right">
+<<<<<<< HEAD
                 <h1>KELOMPOK 1 COYY</h1>
                 <br />
                 <h2 style="color:#4ec5c1;">
@@ -37,6 +62,36 @@ function addContent(params) {
             <div class="left">
                 <img src="assets/image.png" alt="" width="500px" height="375px" />
             </div>
+=======
+            <h1>KELOMPOK 1 COYY</h1>
+            <br />
+            <h2 style="color: #4ec5c1">
+                Dikerjakan Oleh Orang-Orang <br />
+                Yang Tidak Bisa Desain
+            </h2>
+            <br />
+            <p>
+                Lorem Ipsum is simply dummy text of the printing and
+                typesetting industry. Lorem Ipsum has been the
+                industry's standard dummys text ever since the 1500s,
+                when an unknown printer took a galley of types and
+                scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged.
+                It was popularised in the 1960s with the release of
+                Letraset sheets containing Lorem Ipsum passages,
+            </p>
+        </div>
+
+        <div class="left">
+            <img
+                src="assets/image.png"
+                alt=""
+                width="500px"
+                height="375px"
+            />
+        </div>
+>>>>>>> 6e8dd41 (update js, navbar)
         `;
     }
 }
